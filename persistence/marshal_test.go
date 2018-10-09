26e47f806cb9e0f5d860116423c4ded42d962107
Zymurge <zymurge@gmail.com>
2018-10-08 18:58:14 -0700

    added generic test class for strangeness in bsoncodec marshalling

---
+++ persistence/marshal_test.go
@@ -0,0 +1,58 @@
+package persistence
+
+/*
+	This test is in place while mongo-go-driver is in alpha. The goal is to verify that the bsoncodec
+	logic is working or not absent of any wordassassin implementation
+*/
+
+import (
+	"github.com/mongodb/mongo-go-driver/bson"
+	"github.com/mongodb/mongo-go-driver/bson/bsoncodec"
+	"github.com/stretchr/testify/require"
+	"testing"
+	"time"
+)
+
+type TestStruct struct {
+	ID      string    `bson:"_id"`
+	aString string    `bson:"astring"`
+	anInt   int32     `bson:"anint"`
+	aTime   time.Time `bson:"atime"`
+}
+
+func TestShowTimeUnmarshalError(t *testing.T) {
+	testTime := time.Now()
+	myStruct := TestStruct{"myID", "a string", 13, testTime}
+	var result TestStruct
+
+	myDoc := bson.NewDocument(
+		bson.EC.String("_id", myStruct.ID),
+		bson.EC.String("astring", myStruct.aString),
+		bson.EC.Int32("anInt", myStruct.anInt),
+		bson.EC.Time("aTime", myStruct.aTime),
+	)
+	docBSON, derr := myDoc.MarshalBSON()
+	if derr != nil {
+		panic("Marshal fail")
+	}
+	marshalBSON, merr := bsoncodec.Marshal(myStruct)
+	if merr != nil {
+		panic("Marshal fail")
+	}
+	uerr := bsoncodec.Unmarshal(docBSON, &result)
+	if uerr != nil {
+		panic("Unmarshal fail")
+	}
+
+	t.Run("Marshal from Document and interface match", func(t *testing.T) {
+		require.Equal(t, docBSON, marshalBSON)
+	})
+	t.Run("Unmarshal works for string and int", func(t *testing.T) {
+		require.Equal(t, myStruct.ID, result.ID)           // passes
+		require.Equal(t, myStruct.anInt, result.anInt)     // passes
+		require.Equal(t, myStruct.aString, result.aString) // passes
+	})
+	t.Run("Unmarshal works for time", func(t *testing.T) {
+		require.Equal(t, myStruct.aTime, result.aTime) // fails due to rounding on nsec
+	})
+}
